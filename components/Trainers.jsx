0d941b35112eb860accf7ff7a60493848dc860d4
vijaykumar42 <agentvijaykumar@gmail.com>
2024-03-02 00:12:11 +0530

    updated

--- components/Trainers.jsx
+++ components/Trainers.jsx
@@ -32,7 +32,7 @@ export default function Trainers() {
           <Image
             className="rounded-md"
             src="/hero-logo.webp"
-            width={150}
+            width={200}
             height={150}
           />
           <h1 className="text-lg font-medium">Mary Shobha</h1>
@@ -41,8 +41,8 @@ export default function Trainers() {
         <div className="flex flex-col w-full justify-center items-center border m-4 rounded-lg shadow-md p-4 space-y-3 hover:bg-slate-100">
           <Image
             className="rounded-md"
-            src="/hero-logo.webp"
-            width={150}
+            src="/male-trainer.webp"
+            width={200}
             height={150}
           />
           <h1 className="text-lg font-medium">Karthik</h1>
